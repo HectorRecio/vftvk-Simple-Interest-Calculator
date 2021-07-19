8f7ac1175c11aac4b862f338c1bb0885e3cd3067
HectorRecio <hector@peoople.app>
2021-07-19 18:57:35 +0000

    otro

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
-   
+    function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
     // convert the 'No of Years' into the actual year in the future
     var year = new Date().getFullYear()+parseInt(years);
 
@@ -15,8 +18,5 @@ function compute()
     document.getElementById("result").innerHTML=
     "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 
-    function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}
+    
 }
