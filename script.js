c039858f67570f4a59e509cae244831be06b3a7a
Hector <hector@peoople.app>
2021-07-20 18:24:24 +0000

    6

--- script.js
+++ script.js
@@ -10,12 +10,19 @@
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
     }
+
     function compute(){  
+        var principal = principal
+        var rate = rate
+        var amount = principal * interest * years
         document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-    }
+    
     
     //positive number validation
     if (principal <= 0) {alert("Enter a positive number")} 
+    }
+    
+    
 
     
 
