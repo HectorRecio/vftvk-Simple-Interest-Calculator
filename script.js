247cd78b3daaa5922cd889440dfac67c42491901
Hector <hector@peoople.app>
2021-07-20 18:38:45 +0000

    6

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = (principal * years * rate)/100;
-    // convert the 'No of Years' into the actual year in the future
-    var year = new Date().getFullYear()+parseInt(years);
-    //reads the value of the range slider and displays it the <span> adjacent to the slider.
-    function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+function compute()
+{
+    if(principal.value < 1){
+        alert("Amount has to be a positive quantity")
+        principal.focus()
+        return
     }
+    let interest = principal.value * years.value * rate.value / 100;
+    yearInFuture = new Date().getFullYear() + parseInt(years.value);
+    result = document.getElementById("result")
+    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
 
-    function compute(){  
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+}
+
+function updateRate(){
+rate.value
+document.getElementById("rateLabel").innerText = rate.value + "%"}
+
+finishPage = () =>{
+    var rate = document.getElementById("rate");
+    var principal = document.getElementById("principal");
+    var years = document.getElementById("years");
+    years = document.getElementById("years");
+    for(i=1; i<=10; i++){
+    	let element = document.createElement("option")
+    	element.innerText = i
+    	element.setAttribute("value", i)
+    	years.appendChild(element)
     }
-    
-    //positive number validation
-    if (principal <= 0) {alert("Enter a positive number")} 
-    
-
+}
     
 
 
