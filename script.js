002344726e995f44174cb4fe95ad940ad74ffe99
Hector <hector@peoople.app>
2021-07-20 16:59:55 +0000

    5

--- script.js
+++ script.js
@@ -5,16 +5,27 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
 
-    function updateRate() {
+      // convert the 'No of Years' into the actual year in the future
+    var year = new Date().getFullYear()+parseInt(years);
+     //reads the value of the range slider and displays it the <span>adjacent to the slider.
+    
+     function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
     }
+
     //text display
-    document.getElementById("result").innerHTML=
-    "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    function updateResult(){
     
-    // convert the 'No of Years' into the actual year in the future
-    var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML=
+    "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
+    }
+    
+    
+    
+    
+  
 
     //positive number validation
     if (principal <= 0) {alert("Enter a positive number")} 
